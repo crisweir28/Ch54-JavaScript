399b1534b25d13cbfe87c3dacb4eb4131c4e7e95
cris lazcano <lazcanocristobacevedo@gmail.com>
2025-05-12 13:37:32 -0600

    feat: se agregaron botones en el index

---
+++ web07-Js_Intro/ejercicios/myScript.js
@@ -0,0 +1,12 @@
+function saludar(){
+    alert("ya casi nos vamos juasjuas xD ");
+}
+
+function bodyColor (){
+    document.body.style.backgroundColor = "#6600FF" ;
+}
+
+function color(b) {
+    document.body.style.backgroundColor = b;
+}
+
