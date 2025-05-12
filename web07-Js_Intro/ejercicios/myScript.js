0048069507e53162e486f9c139bee3b5974331ae
cris lazcano <lazcanocristobacevedo@gmail.com>
2025-05-12 14:21:57 -0600

    feat: se agregaron botones en el index

--- web07-Js_Intro/ejercicios/myScript.js
+++ web07-Js_Intro/ejercicios/myScript.js
@@ -10,3 +10,20 @@ function color(b) {
     document.body.style.backgroundColor = b;
 }
 
+function changeToGreenColor(){
+    const element = document.getElementById("greenParagraph");
+    console.log(element);
+    element.style.color= "green";
+}
+
+function changeColor(element, color){
+    element.style.color = color;
+}
+
+function setUp(){
+    document.getElementById("title").style.color ="blue";
+    document.getElementById("title").style.fontSize= "50px";
+
+}
+
+setUp();
